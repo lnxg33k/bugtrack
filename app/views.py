de1899191d3ba3b9eac3aca5e20095a40b9865bf
lnxg33k <ahmedelantry@gmail.com>
2015-10-03 04:01:06 +0300

    Finished commenting section

--- app/views.py
+++ app/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import (
+    render_to_response, get_object_or_404, HttpResponseRedirect)
+from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from app.models import Assessment, Stakeholder, Finding
+from app.models import Assessment, Stakeholder, Finding, Comment
+from app.forms import CommentForm
+from django.contrib import messages
 # Create your views here.
 
 
 def profile(request):
     stakeholder = Stakeholder.objects.get(username=request.user)
     assessments = Assessment.objects.filter(
-                        stakeholders=stakeholder).order_by('-ends_at')
+        stakeholders=stakeholder).order_by('-ends_at')
     # findings = Finding.objects.filter(assessment=assessments)
     paginator_assessments = Paginator(assessments, 10)
     # paginator_findings = Paginator(findings, 1)
@@ -57,6 +61,31 @@ def view_finding(request, assessment_slug, slug):
     finding = get_object_or_404(
         Finding, slug=slug, assessment=assessment
     )
+    comment_form = CommentForm()
     return render_to_response(
         'view_finding.html', locals(),
         context_instance=RequestContext(request))
+
+
+def add_comment(request, finding_id, parent_id=None):
+    if request.method == "POST":
+        stakeholder = Stakeholder.objects.get(username=request.user)
+        finding = get_object_or_404(
+            Finding, pk=finding_id, assessment__stakeholders=stakeholder)
+        print 'passed'
+        comment = Comment(
+            finding=finding, commenter=stakeholder,
+            parent_id=parent_id)
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Comment was successfully added.")
+        else:
+            messages.error(request, form.errors.as_text())
+    else:
+        form = CommentForm()
+    return HttpResponseRedirect(reverse(
+        "finding_detail", kwargs={
+            'assessment_slug': finding.assessment.slug, 'slug': finding.slug
+        }
+    ))
