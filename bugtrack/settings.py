9910874eadd47c14b5bef1c39206c9c9c9890ad0
lnxg33k <ahmedelantry@gmail.com>
2016-07-22 21:29:47 -0600

    Add the settings file

--- bugtrack/settings.py
+++ bugtrack/settings.py
@@ -33,15 +33,33 @@ else:
 if DEBUG:
     ALLOWED_HOSTS = ["*"]
     STATIC_URL = '/static/'
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     CAPTCHA_CHALLENGE_FUNCT = 'captcha.helpers.random_char_challenge'
     CAPTCHA_NOISE_FUNCTIONS = ('captcha.helpers.noise_null', )
     CAPTCHA_LETTER_ROTATION = None
+    DB_ENGINE = 'django.db.backends.sqlite3'
+    DB_NAME = os.path.join(BASE_DIR, 'db.sqlite3')
 else:
     if socket.gethostname() == 'ubuntu':
         ALLOWED_HOSTS = ['.bugtrack.com']
+        DEFAULT_FROM_EMAIL = "info@bugtrack.com"
+        DB_ENGINE = 'django.db.backends.sqlite3'
+        DB_NAME = os.path.join(BASE_DIR, 'db.sqlite3')
     else:
+        # Security
+        CSRF_COOKIE_NAME = "cookieXSRF"
+        CSRF_COOKIE_SECURE = True
+        CSRF_COOKIE_HTTPONLY = True
+        SECURE_BROWSER_XSS_FILTER = True
+        SECURE_CONTENT_TYPE_NOSNIFF = True
+
         ALLOWED_HOSTS = ["bugtrack.online", "www.bugtrack.online"]
+        DEFAULT_FROM_EMAIL = "info@bugtrack.online"
+
+        DB_ENGINE = 'django.db.backends.mysql'
+        DB_NAME = 'bugtrack'
+        DB_USER = 'bugtrackuser'
+        DB_PASSWORD = r"?5PJ6Q'bGv,@X&yV_f3j"
+
     ADMINS = (('Ahmed', 'ahmedelantry@gmail.com'), )
     STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
     STATIC_URL = '/assets/'
@@ -59,7 +77,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'ckeditor',
+    'django_bleach',
     'app',
     'captcha',
     'preventconcurrentlogins',
@@ -105,11 +125,17 @@ WSGI_APPLICATION = 'bugtrack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': DB_ENGINE,
+        'NAME': DB_NAME,
     }
 }
-
+if not DB_ENGINE.endswith('sqlite3'):
+    DATABASES['default'].update({
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    })
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -137,7 +163,7 @@ MEDIA_URL = '/media/'
 
 GRAPPELLI_ADMIN_TITLE = "bugTrack"
 
-CKEDITOR_UPLOAD_PATH = "uploads/"
+# CKEDITOR_UPLOAD_PATH = "uploads/"
 
 CKEDITOR_CONFIGS = {
     'default': {
@@ -159,10 +185,49 @@ CKEDITOR_CONFIGS = {
         # 'autoParagraph': False,
     },
 }
+# Which HTML tags are allowed
+BLEACH_ALLOWED_TAGS = [
+    'p', 'b', 'i', 'u', 'em', 'strong', 'a',
+    'pre', 'code', 'ol', 'ul', 'li']
+
+# Which HTML attributes are allowed
+BLEACH_ALLOWED_ATTRIBUTES = ['href', 'title', 'style']
+
+# Which CSS properties are allowed in 'style' attributes (assuming
+# style is an allowed attribute)
+BLEACH_ALLOWED_STYLES = [
+    'font-family', 'font-weight', 'text-decoration', 'font-variant']
+
+# Strip unknown tags if True, replace with HTML escaped characters if
+# False
+BLEACH_STRIP_TAGS = True
+
+# Strip comments, or leave them in.
+BLEACH_STRIP_COMMENTS = False
+
+BLEACH_DEFAULT_WIDGET = 'wysiwyg.widgets.WysiwygWidget'
+
+# To send debug messages
+SERVER_EMAIL = "debug@bugtrack.online"
 
 # Registration section
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 ACCOUNT_ACTIVATION_DAYS = 7
 PASSWORD_MIN_LENGTH = 8
+
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'info@bugtrack.online'
+EMAIL_HOST_PASSWORD = r'4`sGRXW]c=J\3;Gfs!{a'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+
+# EMAIL_HOST_USER = 'ahmedelantry@gmail.com'
+# EMAIL_HOST_PASSWORD = 'xxx'
+
 # SEND_ACTIVATION_EMAIL = False
 
 CAPTCHA_OUTPUT_FORMAT = u'%(hidden_field)s%(image)s %(text_field)s'
@@ -172,3 +237,5 @@ LOGIN_REDIRECT_URL = '/'
 AUTHENTICATION_BACKENDS = [
     'django-dual-authentication.backends.DualAuthentication'
     ]
+
+SITE_ID = 1
